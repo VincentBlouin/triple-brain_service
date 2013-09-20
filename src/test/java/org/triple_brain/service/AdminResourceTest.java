c16b1f28b62f24ae08c7a19e43d51a403f4755b2
Vincent Blouin <vincent.blouin@gmail.com>
2013-09-20 17:17:28 -0400

    trying to fix cannot reindex edges when reindexing whole graph

---
+++ src/test/java/org/triple_brain/service/AdminResourceTest.java
@@ -0,0 +1,15 @@
+package org.triple_brain.service;
+
+import org.junit.Test;
+import org.triple_brain.service.utils.GraphManipulationRestTest;
+
+/*
+* Copyright Mozilla Public License 1.1
+*/
+public class AdminResourceTest extends GraphManipulationRestTest {
+
+    @Test
+    public void edges_are_indexed_when_indexing_whole_graph(){
+
+    }
+}
