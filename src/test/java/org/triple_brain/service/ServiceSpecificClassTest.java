84b6c698b0604f877eb265fa78d9cdb17950c141
Vincent Blouin <vincent.blouin@gmail.com>
2015-04-07 11:15:56 -0400

    password_cant_be_too_short

--- src/test/java/org/triple_brain/service/ServiceSpecificClassTest.java
+++ src/test/java/org/triple_brain/service/ServiceSpecificClassTest.java
@@ -8,6 +8,7 @@ import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+@Ignore
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         UserPasswordResourceTest.class
