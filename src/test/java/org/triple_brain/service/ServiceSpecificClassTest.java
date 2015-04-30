9f1438e116f93ecde605d4701cfb81a9aefc8772
Vincent Blouin <vincent.blouin@gmail.com>
2015-04-30 09:54:20 -0400

    service tests

--- src/test/java/org/triple_brain/service/ServiceSpecificClassTest.java
+++ src/test/java/org/triple_brain/service/ServiceSpecificClassTest.java
@@ -7,8 +7,11 @@ package org.triple_brain.service;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+
+@Ignore
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         SchemaPropertyResourceTest.class
 })
-public class ServiceSpecificClassTest extends ServiceTestRunner{}
+public class ServiceSpecificClassTest extends ServiceTestRunner {
+}
