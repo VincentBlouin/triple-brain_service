40d2b17a5557071db6c44bf9fba47ead70c56c08
Vincent Blouin <vincent.blouin@gmail.com>
2015-10-17 11:54:58 -0400

    services tests

--- src/test/java/guru/bubl/service/resources/ServiceSpecificClassTest.java
+++ src/test/java/guru/bubl/service/resources/ServiceSpecificClassTest.java
@@ -5,10 +5,10 @@
 package guru.bubl.service.resources;
 
 import guru.bubl.service.resources.vertex.VertexOwnedSurroundGraphResouceTest;
-import guru.bubl.service.resources.vertex.VertexResourceTest;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+@Ignore
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         VertexOwnedSurroundGraphResouceTest.class
