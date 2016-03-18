2a0743dffe9af4cd833f0236dc7443fb940f73ae
Vincent Blouin <vincent.blouin@gmail.com>
2016-03-18 14:21:45 -0400

    owner of identification can rename label

--- src/test/java/guru/bubl/service/resources/ServiceSpecificClassTest.java
+++ src/test/java/guru/bubl/service/resources/ServiceSpecificClassTest.java
@@ -10,6 +10,7 @@ import guru.bubl.service.resources.vertex.VertexSuggestionResourceTest;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+@Ignore
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         IdentificationResourceTest.class
