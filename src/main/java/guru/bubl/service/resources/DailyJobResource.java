5597dc456fa5421aa76b63e16fffc2ba85897125
Vincent Blouin <vincent.blouin@gmail.com>
2017-08-05 21:45:53 -0400

    daily job logs

--- src/main/java/guru/bubl/service/resources/DailyJobResource.java
+++ src/main/java/guru/bubl/service/resources/DailyJobResource.java
@@ -13,6 +13,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Path("/daily-job")
 public class DailyJobResource {
@@ -24,6 +25,7 @@ public class DailyJobResource {
     @GraphTransactional
     @Path("/")
     public Response doDailyJob(){
+        System.out.println("Current DateTime: " + LocalDateTime.now());
         System.out.println("daily job started");
         Instant start = Instant.now();
         try{
@@ -34,6 +36,7 @@ public class DailyJobResource {
         }finally{
             Instant end = Instant.now();
             System.out.println(Duration.between(start, end));
+            System.out.println("Current DateTime: " + LocalDateTime.now());
         }
         return Response.ok().build();
     }
